ddeb1a22d88155f71fb106065eb8d56aeb276835
Alexey <alexragnarek@gmail.com>
2023-01-31 21:07:50 +0500

    ex41 is complited!

---
+++ ex41/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Clear();
+Console.Write("Введите колличество чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+  Console.Write($"Введите число {i + 1}: ");
+  int num = Convert.ToInt32(Console.ReadLine());
+  if (num > 0) count ++;
+}
+Console.WriteLine($"Колличество чисел больше 0: {count}");
